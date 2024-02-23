e2c818e0e7aea81fcc2a0e1754068a0bf6e9a79b
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-02-23 10:50:04 +0000

    feat: done basically menu area and header

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className="md:text-2xl lg:text-3xl font-bold">
+    <div className="sm:text-2xl lg:text-3xl font-bold">
       <Link href="/">
         <span className="text-main">Explora</span>Tur
       </Link>
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,4 +1,8 @@
+"use client";
+
 import { MenuIcon } from "lucide-react";
+import { useMediaQuery } from "usehooks-ts";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 const links = [
   {
@@ -19,21 +23,48 @@ const links = [
   },
 ];
 
-const mobile = true;
-
 export const Menu = () => {
+  // TODO: add navigation menu from shacn-ui
+  const matches = useMediaQuery("(max-width: 600px)");
+
+  // TODO: refact visually all menu section
   return (
-    <ul className="text-xs md:text-base flex transition font-bold">
-      {links?.map((obj) => (
-        <li
-          key={obj.linkName}
-          className="hover:text-main hover:scale-105 transition"
-        >
-          <a className="px-1 md:px-2" href={obj.src}>
-            {obj.linkName}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <div>
+      {matches && (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MenuIcon />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              Aventureiros
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Condutores
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Contato
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Sobre nós
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
+      {!matches && (
+        <ul className="text-xs md:text-base flex transition font-bold">
+        {links?.map((obj) => (
+          <li
+            key={obj.linkName}
+            className="hover:text-main hover:scale-105 transition"
+          >
+            <a className="px-1 md:px-2" href={obj.src}>
+              {obj.linkName}
+            </a>
+          </li>
+        ))}
+      </ul>
+      )}
+    </div>
   );
 };
