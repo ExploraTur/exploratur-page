504fbb1d6f29461c7dc3bd61706732bbd207b8e7
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-02-24 13:54:19 +0000

    style: fixed center style and added SearchArea component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "@/components/Hero/hero";
+import { SearchArea } from "@/components/SearchArea/SearchArea";
 import { Header } from "@/components/header";
 
 export default function Main() {
   return (
     <>
     <Header />
-    <main className='flex justify-center overflow-visible'>
+    <main className='flex flex-col items-center overflow-visible'>
       <Hero />
+      <SearchArea />
     </main>
     </>
   );
