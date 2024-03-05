df99b33a03f379b81a076a876606b242f60018ba
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-03-05 13:32:20 +0000

    feat: added carousel and feedbacks section

---
+++ components/Feedbacks/commentArea.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+interface CommentAreaProps {
+  userPhoto: string;
+  userName: string;
+  comment: string;
+}
+
+export const CommentArea = ({
+  userName,
+  userPhoto,
+  comment,
+}: CommentAreaProps) => {
+  return (
+    <article className="bg-white/90 rounded-lg min-h-min py-10">
+      <div className="flex flex-col justify-center items-center gap-y-4 px-4">
+        <div className='relative h-[145px] w-[145px]'>
+          <Image
+            src={userPhoto}
+            alt="Foto do cliente"
+            fill
+            className="shadow-xl shadow-main/60 rounded-full absolute object-contain-cover top-0 left-0"
+          />
+        </div>
+        <h4 className="text-xl font-semibold">{userName}</h4>
+        <p className="text-gray-500 max-w-[800px]">{comment}</p>
+      </div>
+    </article>
+  );
+};
--- components/Feedbacks/feedbacks.tsx
+++ components/Feedbacks/feedbacks.tsx
@@ -1,5 +1,39 @@
-import { Star } from "lucide-react";
 import { Ratings } from "./ratings";
+import { CommentArea } from "./commentArea";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+
+const usersInfo = [
+  {
+    userPhoto: "/userPhoto.jpg",
+    userName: "Philipha Esteves",
+    comment:
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+  },
+  {
+    userPhoto: "/userPhoto.jpg",
+    userName: "Philipha Esteves",
+    comment:
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+  },
+  {
+    userPhoto: "/userPhoto.jpg",
+    userName: "Philipha Esteves",
+    comment:
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+  },
+  {
+    userPhoto: "/userPhoto.jpg",
+    userName: "Philipha Esteves",
+    comment:
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+  },
+];
 
 export const Feedbacks = () => {
   return (
@@ -7,10 +41,23 @@ export const Feedbacks = () => {
       <h3 className="font-bold text-4xl text-submain">
         O que dizem nossos exploradores?
       </h3>
-      <p className="text-gray-300 mt-1">Média de classificação dos clientes</p>
-      <div className=''>
-        <Ratings />
-      </div>
+      <p className="text-gray-300 mt-4">Média de classificação dos clientes</p>
+      <Ratings />
+      <Carousel className="w-full">
+        <CarouselContent className='flex justify-start max-w-[70%] xl:max-w-[52%] mx-auto'>
+          {usersInfo.map((user, i) => (
+            <CarouselItem key={i} className='max-w-5xl'>
+              <CommentArea
+                userName={user.userName}
+                userPhoto={user.userPhoto}
+                comment={user.comment}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </section>
   );
 };
