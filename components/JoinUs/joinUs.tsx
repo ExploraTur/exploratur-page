ba4fe3f5797c0d8b80eb5828457cfaa3e1059dcd
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-03-07 17:31:40 +0000

    style: changed spacings

--- components/JoinUs/joinUs.tsx
+++ components/JoinUs/joinUs.tsx
@@ -2,13 +2,13 @@ import { Button } from "../ui/button";
 
 export const JoinUs = () => {
   return (
-    <section className="w-full mb-10">
+    <section className="w-full mt-10 mb-20">
       <div className="rounded-lg bg-main text-white max-w-5xl flex flex-col justify-center items-center mx-auto text-center">
         <div className='my-12'>
           <h3 className='font-bold text-xl'>Explorar novos lugares nunca foi tão simples.</h3>
           <p>Junte-se a nós e aventure-se.</p>
         </div>
-        <Button size='lg' className='bg-white font-bold text-main-dark mb-14 hover:text-white hover:bg-main-dark'>Buscar aventura</Button>
+        <Button size='lg' className='bg-white font-bold text-main-dark mb-12 hover:text-white hover:bg-main-dark'>Buscar aventura</Button>
       </div>
     </section>
   );
