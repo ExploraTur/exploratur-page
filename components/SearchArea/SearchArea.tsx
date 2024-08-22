9a34b1e635c8ae8fa3cb7a7cffb9783ff8d0e447
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-08-22 17:36:08 +0100

    refact: changed Lucide icons for React Icons lib

--- components/SearchArea/SearchArea.tsx
+++ components/SearchArea/SearchArea.tsx
@@ -1,27 +1,41 @@
-import { Pin, Calendar, UsersRound, Search } from "lucide-react";
+import { Button } from "../ui/button";
 import { SearchLabel } from "./searchLabel";
+import { CiSearch } from "react-icons/ci";
 
 export const SearchArea = () => {
   return (
-    <section className="w-full flex justify-center border">
+    <section className="w-full flex justify-center pt-26">
       <div className="bg-white/90 max-w-[1440px] flex justify-between items-center gap-x-3 mx-4 py-6 px-8 rounded-lg shadow-lg shadow-main/20">
         <div>
-          <h3 className="font-medium">Localidade</h3>
-          <SearchLabel placeholder="Onde quer ir?" icon={Pin} />
+          <h3 className="text-gray-600 font-medium">Localidade</h3>
+          <SearchLabel placeholder="Onde quer ir?" iconName={"CiLocationOn"} />
         </div>
         <div>
-          <h3 className="font-medium">Data</h3>
-          <SearchLabel placeholder="Quando quer ir?" icon={Calendar} />
+          <h3 className="text-gray-600 font-medium">Data</h3>
+          <SearchLabel
+            placeholder="Quando quer ir?"
+            iconName={"CiCalendarDate"}
+            // type="date"
+          />
         </div>
         <div>
-          <h3 className="font-medium">Exploradores</h3>
+          <h3 className="text-gray-600 font-medium">Exploradores</h3>
           <SearchLabel
             placeholder="Quantas pessoas?"
-            icon={UsersRound}
+            iconName={"PiUsersLight"}
             type="number"
           />
         </div>
-        <Search size={30} />
+        <div>
+          <h3 className="text-gray-600 font-medium mb-2">Buscar</h3>
+          <Button
+            title="Buscar Viagens"
+            variant="main"
+            className="focus-visible:ring-main mb-2 py-6 focus-visible:ring-offset-4 "
+          >
+            <CiSearch size={36} fill="white" />
+          </Button>
+        </div>
       </div>
     </section>
   );
--- components/SearchArea/searchLabel.tsx
+++ components/SearchArea/searchLabel.tsx
@@ -1,23 +1,37 @@
-import { LucideIcon } from "lucide-react";
+import { CiCalendarDate, CiLocationOn, CiSearch } from "react-icons/ci";
+import { PiUsersLight } from "react-icons/pi";
+import { IconType } from "react-icons/lib";
+
 import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+
+const iconMap = {
+  CiCalendarDate,
+  CiLocationOn,
+  PiUsersLight,
+  CiSearch,
+};
 
 interface SearchLabelProps {
-  icon: LucideIcon;
+  iconName: keyof typeof iconMap;
   placeholder?: string;
   type?: string;
 }
 
 export const SearchLabel = ({
-  icon: Icon,
+  iconName,
   placeholder,
   type,
 }: SearchLabelProps) => {
+  const IconComponent: IconType = iconMap[iconName];
+
   return (
-    <div className="flex px-2 py-2 items-center rounded-md gap-x-2">
-      <Icon size={30} fill="#2c94fc"/>
+    <div className="group-hover:opacity-100 focus-within:opacity-100 transition-opacity flex px-2 py-2 items-center rounded-md gap-x-2">
+      {IconComponent && <IconComponent size={30} fill="#2c94fc" />}
       <Input
+        min={0}
         placeholder={placeholder}
-        type={type === "number" ? "number" : "string"}
+        type={type}
         className="-ml-12 pl-12 placeholder:text-gray-400 bg-transparent"
       />
     </div>
