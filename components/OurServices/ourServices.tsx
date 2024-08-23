d64b6ee772beff5b2594a5e9a7d4555433a736bb
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-08-23 18:38:41 +0100

    feat: done basic structure and style in OurServices section

--- components/OurServices/ourServices.tsx
+++ components/OurServices/ourServices.tsx
@@ -1,31 +1,151 @@
 import { BusFront, Key } from "lucide-react";
+import { MdManageSearch } from "react-icons/md";
 
 import { OurServicesCard } from "./ourServicesCard";
+import { montserrat } from "@/app/layout";
 
 export const OurServices = () => {
   return (
-    <section className="mt-16 mb-10 text-center">
-      <h2 className=" font-bold text-4xl text-submain">Nossos Serviços</h2>
-      <p className="text-sm text-gray-500 mt-1 px-2">
-        Fornecemos uma plataforma integrada que une aventureiros e condutores{" "}
+    <section className="max-w-[1440px] text-center mx-auto pt-48 pb-32">
+      <h2 className={`${montserrat.className} font-bold text-5xl text-submain`}>
+        Nossos Serviços
+      </h2>
+      <p className="text-gray-600 mt-6">
+        Seja você um <strong>explorador</strong> ou{" "}
+        <strong>condutor de viagens</strong>, fornecemos uma plataforma
+        integrada com serviços específicos às suas necessidades.
       </p>
-      <article className="flex flex-wrap justify-center mx-2 gap-4 my-10">
-        <OurServicesCard
-          src={"/bg.png"}
-          title="Para aventureiros"
-          subtitle="Se você busca a próxima aventura, conhecer um local, fazer uma excursão."
-          url="/search"
-          ctaBtn="Buscar aventura"
-          icon={BusFront}
-        />
-        <OurServicesCard
-          src={"/bgBus.png"}
-          title="Para condutores"
-          subtitle="Se é o condutor, registre-se e ofereça suas excursões personalizadas."
-          url="/login"
-          ctaBtn="Registrar-se"
-          icon={Key}
-        />
+
+      <article className="mx-8 my-20">
+        <div id="#explorers" className="flex flex-wrap justify-center gap-20">
+          <OurServicesCard
+            src={"/bg.png"}
+            title="Para aventureiros"
+            subtitle="Se você busca a próxima aventura, conhecer um local, fazer uma excursão."
+            url="/search"
+            ctaBtn="Buscar aventura"
+            icon={"FaPersonWalkingLuggage"}
+            alt="Área de redirecionamento para exploradores"
+          />
+
+          <div className="max-w-[500px] text-left flex gap-y-6 flex-col items-center justify-around">
+            <h3
+              className={`${montserrat.className} relative text-2xl self-start font-bold text-neutral-700 mb-12`}
+            >
+              Nossos serviços para Aventureiros
+              <span className="w-16 h-2 bg-main rounded-full absolute -bottom-4 left-0" />
+            </h3>
+            <div className="flex gap-x-6">
+              <div className="flex self-start p-4 shadow-main/70 shadow-md rounded-full">
+                <MdManageSearch size="3rem" fill="#2c94fc" />
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-4 text-neutral-700">
+                  Busca Facilitada
+                </h4>
+                <p className="text-neutral-500 font-medium">
+                  Tudo que precisa fazer é apenas escolher o local que deseja
+                  explorar e a data da aventura
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-x-6">
+              <div className="flex self-start p-4 shadow-main/70 shadow-md rounded-full">
+                <MdManageSearch size="3rem" fill="#2c94fc" />
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-4 text-neutral-700">
+                  Busca Facilitada
+                </h4>
+                <p className="text-neutral-500 font-medium">
+                  Tudo que precisa fazer é apenas escolher o local que deseja
+                  explorar e a data da aventura
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-x-6">
+              <div className="flex self-start p-4 shadow-main/70 shadow-md rounded-full">
+                <MdManageSearch size="3rem" fill="#2c94fc" />
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-4 text-neutral-700">
+                  Busca Facilitada
+                </h4>
+                <p className="text-neutral-500 font-medium">
+                  Tudo que precisa fazer é apenas escolher o local que deseja
+                  explorar e a data da aventura
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="#drivers" className="mt-40 flex flex-wrap justify-center gap-20">
+          <div className="max-w-[500px] text-left flex gap-y-6 flex-col items-center justify-around">
+            <h3
+              className={`${montserrat.className} relative text-2xl self-start font-bold text-neutral-700 mb-12`}
+            >
+              Nossos serviços para Condutores
+              <span className="w-16 h-2 bg-main rounded-full absolute -bottom-4 left-0" />
+            </h3>
+            <div className="flex gap-x-6">
+              <div className="flex self-start p-4 shadow-main/70 shadow-md rounded-full">
+                <MdManageSearch size="3rem" fill="#2c94fc" />
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-4 text-neutral-700">
+                  Busca Facilitada
+                </h4>
+                <p className="text-neutral-500 font-medium">
+                  Tudo que precisa fazer é apenas escolher o local que deseja
+                  explorar e a data da aventura
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-x-6">
+              <div className="flex self-start p-4 shadow-main/70 shadow-md rounded-full">
+                <MdManageSearch size="3rem" fill="#2c94fc" />
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-4 text-neutral-700">
+                  Busca Facilitada
+                </h4>
+                <p className="text-neutral-500 font-medium">
+                  Tudo que precisa fazer é apenas escolher o local que deseja
+                  explorar e a data da aventura
+                </p>
+              </div>
+            </div>
+
+            <div className="flex gap-x-6">
+              <div className="flex self-start p-4 shadow-main/70 shadow-md rounded-full">
+                <MdManageSearch size="3rem" fill="#2c94fc" />
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold mb-4 text-neutral-700">
+                  Busca Facilitada
+                </h4>
+                <p className="text-neutral-500 font-medium">
+                  Tudo que precisa fazer é apenas escolher o local que deseja
+                  explorar e a data da aventura
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <OurServicesCard
+            src={"/bgBus.png"}
+            title="Para condutores"
+            subtitle="Se é condutor, registre-se e ofereça suas excursões personalizadas."
+            url="/login"
+            ctaBtn="Registrar-se"
+            icon={"BiLogIn"}
+            alt="Área para cadastro de novos condutores"
+          />
+        </div>
       </article>
     </section>
   );
--- components/OurServices/ourServicesCard.tsx
+++ components/OurServices/ourServicesCard.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import { FaPersonWalkingLuggage } from "react-icons/fa6";
+import { BiLogIn } from "react-icons/bi";
+import Link from "next/link";
+import { IconType } from "react-icons/lib";
 
 import { Button } from "../ui/button";
-import { LucideIcon } from "lucide-react";
-import Link from "next/link";
+
+const iconMap = {
+  FaPersonWalkingLuggage,
+  BiLogIn,
+};
 
 interface OurServicesCard {
   src: string;
@@ -10,7 +17,8 @@ interface OurServicesCard {
   subtitle: string;
   url: string;
   ctaBtn: string;
-  icon: LucideIcon;
+  icon: keyof typeof iconMap;
+  alt: string;
 }
 
 export const OurServicesCard = ({
@@ -18,23 +26,28 @@ export const OurServicesCard = ({
   title,
   subtitle,
   url,
-  icon: Icon,
+  icon,
   ctaBtn,
+  alt,
 }: OurServicesCard) => {
+  const IconComponent: IconType = iconMap[icon];
+
   return (
-    <div className="rounded-lg min-h-[500px] max-w-[374px] relative shadow-md shadow-main-dark/30">
+    <div className="rounded-lg h-[470px] max-w-[374px] relative">
       <Image
         src={src}
         fill
-        alt="Serviços para aventureiros"
+        alt={alt}
         className="rounded-lg -z-10 absolute bg-cover"
       />
       <div className="px-10 text-left text-white h-full flex flex-col justify-end gap-2 pb-4">
-        <h3 className="font-bold text-xl border-b-2 border-submain">{title}</h3>
-        <p className="max-w-[300px]">{subtitle}</p>
-        <Button variant="main" className="gap-x-2">
+        <h3 className="font-bold text-xl border-b-2 border-submain">
+          {title}
+        </h3>
+        <p className="max-w-[300px] font-semibold">{subtitle}</p>
+        <Button variant="main" className="gap-x-2 rounded-lg">
           <Link href={url}>{ctaBtn}</Link>
-          <Icon />
+          {IconComponent && <IconComponent size={20} />}
         </Button>
       </div>
     </div>
