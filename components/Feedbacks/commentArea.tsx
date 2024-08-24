1249c5fbd86abcbdd9e3d87c7f3a8a14fa4032a0
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-08-24 15:12:38 +0100

    style: increased vertical padding at feedback area and comment area also included a bg image

--- components/Feedbacks/commentArea.tsx
+++ components/Feedbacks/commentArea.tsx
@@ -12,18 +12,20 @@ export const CommentArea = ({
   comment,
 }: CommentAreaProps) => {
   return (
-    <article className="bg-white/90 rounded-lg min-h-min py-4 sm:py-10">
+    <article className="bg-white relative rounded-lg px-6 min-h-min py-10 sm:py-20">
       <div className="flex flex-col justify-center items-center gap-y-4 px-4">
-        <div className='relative h-[145px] w-[145px]'>
+      <div className="z-0 absolute top-0 left-0 h-screen rotate-90 opacity-5 w-full bg-line-pattern bg-cover" />
+
+        <div className='relative h-[170px] w-[170px]'>
           <Image
             src={userPhoto}
             alt="Foto do cliente"
             fill
-            className="shadow-xl shadow-main/60 rounded-full absolute object-contain-cover top-0 left-0"
+            className="shadow-lg shadow-main/60 rounded-full absolute object-contain-cover top-0 left-0"
           />
         </div>
-        <h4 className="text-lg sm:text-xl font-semibold">{userName}</h4>
-        <p className="text-gray-500 text-sm sm:text-base max-w-[800px]">{comment}</p>
+        <h4 className="text-lg sm:text-xl text-gray-700 font-semibold">{userName}</h4>
+        <p className="text-gray-500 text-lg max-w-[700px]">{comment}</p>
       </div>
     </article>
   );
--- components/Feedbacks/feedbacks.tsx
+++ components/Feedbacks/feedbacks.tsx
@@ -7,25 +7,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "../ui/carousel";
+import { montserrat } from "@/app/layout";
 
 const usersInfo = [
   {
     userPhoto: "/userPhoto.jpg",
     userName: "Philipha Esteves",
     comment:
-      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas.",
   },
   {
     userPhoto: "/userPhoto.jpg",
     userName: "Philipha Esteves",
     comment:
-      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas.",
   },
   {
     userPhoto: "/userPhoto.jpg",
     userName: "Philipha Esteves",
     comment:
-      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas",
+      "Eu sou agente de viagens e o site me ajuda a encontrar locais interessantes tanto para pessoas que estão bucando suas primeiras viagens quanto para aventureiros e pessoas mais experientes além de fornecer viagens agradáveis e baratas.",
   },
   {
     userPhoto: "/userPhoto.jpg",
@@ -37,16 +38,22 @@ const usersInfo = [
 
 export const Feedbacks = () => {
   return (
-    <section className="text-center w-full py-20 bg-gradient-to-t from-main to-main-dark ">
-      <h3 className="font-bold mx-2 text-2xl sm:text-4xl text-submain">
-        O que dizem nossos exploradores?
-      </h3>
-      <p className="text-gray-300 text-sm sm:mt-4 mt-2">Média de classificação dos clientes</p>
-      <Ratings />
+    <section className="w-full pt-36 pb-60 rounded-2xl bg-gradient-to-t from-main to-main-dark ">
+      <div className="pb-10 text-center">
+        <h2
+          className={`${montserrat.className} font-bold text-5xl text-submain`}
+        >
+          O que dizem nossos exploradores?
+        </h2>
+        <p className="text-gray-300 sm:mt-4 mt-2">
+          Média de classificação dos clientes
+        </p>
+        <Ratings />
+      </div>
       <Carousel>
-        <CarouselContent className='flex justify-start max-w-[70%] xl:max-w-[52%] mx-auto min-w-[290px]'>
+        <CarouselContent className="flex justify-start max-w-[70%] xl:max-w-[52%] mx-auto min-w-[290px]">
           {usersInfo.map((user, i) => (
-            <CarouselItem key={i} className='max-w-5xl'>
+            <CarouselItem key={i} className="max-w-5xl">
               <CommentArea
                 userName={user.userName}
                 userPhoto={user.userPhoto}
