c5e71d697e5045cb9d4a6caa7d2e90cada06d9cf
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-02-25 19:35:37 +0000

    feat: adds initial authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex min-h-screen justify-center items-center">{children}</div>
+  );
+};
+
+export default Layout;
